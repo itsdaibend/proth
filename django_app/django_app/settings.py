a2ae0594fd9f5246b5b66562e737cde66bd821a5
itsdaibend <kotljar.daniel@gmail.com>
2022-06-10 03:16:12 +0300

    [FE] Bootstrap-Modal Windows and Forms for Todo

--- django_app/django_app/settings.py
+++ django_app/django_app/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'todo',
+
+    'widget_tweaks',
 ]
 
 MIDDLEWARE = [
@@ -100,9 +102,9 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = False
+USE_L10N = True
 
-USE_TZ = False
+USE_TZ = True
 
 
 
--- django_app/todo/static/todo/todopage.js
+++ django_app/todo/static/todo/todopage.js
@@ -1,47 +1,14 @@
 // Function for creation modal window.
 $(document).ready(function() {
-    $('a.TodoCreationWindow').click( function(event){
+    $('button.TodoCreationWindow').click( function(){
       
       let status = $(this).attr("id");
       let form = $(".form-group")[0];
       form.status.value = status;
-
-      event.preventDefault();
-      $('#SiteOverlay').fadeIn(297,	function(){
-        $('#TodoCreationWindow') 
-        .css('display', 'block')
-        .animate({opacity: 1}, 198);
-      });
-    });
-  
-    $('#TodoCreationWindow__close, #SiteOverlay').click( function(){
-      $('#TodoCreationWindow').animate({opacity: 0}, 198, function(){
-        $(this).css('display', 'none');
-        $('#SiteOverlay').fadeOut(297);
-      });
     });
   });
 
-// Function for the update modal window.
-$(document).ready(function() {
-  $('button.TodoUpdateWindow').click( function(event){
-    event.preventDefault();
-    $('#SiteOverlay').fadeIn(297,	function(){
-      $('#TodoUpdateWindow') 
-      .css('display', 'block')
-      .animate({opacity: 1}, 198);
-    });
-  });
-
-  $('#TodoUpdateWindow__close, #SiteOverlay').click( function(){
-    $('#TodoUpdateWindow').animate({opacity: 0}, 198, function(){
-      $(this).css('display', 'none');
-      $('#SiteOverlay').fadeOut(297);
-    });
-  });
-});
-
-// Function for moving the Todo's ID to the form action field and for values substitution in update form.
+// Function for update modal window.
 $(function(){
   $(".btn-secondary").on("click", function(){
       var id = $(this).attr("id");
