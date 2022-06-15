4aa257d5c6e1711e3e5d9d0f73936af28280ba5c
itsdaibend <kotljar.daniel@gmail.com>
2022-06-16 00:07:37 +0300

    [BE] 'get' instead of 'get_queryset' method in home page view

--- django_app/django_app/views.py
+++ django_app/django_app/views.py
@@ -1,21 +1,21 @@
-from django.shortcuts import redirect
-from django.views.generic import ListView
+from django.shortcuts import redirect, render
+from django.views import View
 
 from todo.models import Todo
 
 
-class HomePageView(ListView):
+class HomePageView(View):
     model = Todo
     template_name = 'home_page.html'
-    context_object_name = 'todos'
-    extra_context = {'page_title': 'Home'}
+    context = {'page_title': 'Home'}
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
     
-    def get_queryset(self):
-        if not self.request.user.is_authenticated:
+    def get(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
             return redirect('sign_in')
 
-        return Todo.objects.filter(user=self.request.user, status=2)
+        self.context['todos'] = Todo.objects.filter(user=request.user, status=2)
+        return render(request, 'home_page.html', self.context)
