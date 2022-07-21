b52d51183cabf0691bb9f479acb29e8331542827
itsdaibend <kotljar.daniel@gmail.com>
2022-07-21 19:47:38 +0300

    [FE/BE] Phrase Creation Form and modal window(UI) for this form

---
+++ django_app/languages/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import Phrase
+
+
+class PhraseCreationForm(forms.ModelForm):
+    class Meta:
+        model = Phrase
+        fields = ['source_lang', 'target_lang', 'source_text', 'target_text', 'comment', 'label']
--- django_app/languages/views.py
+++ django_app/languages/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import redirect, render
 from django.views import View
 
 from .models import Phrase
+from .forms import PhraseCreationForm
 
 
 class LanguagesPageView(View):
@@ -10,6 +11,18 @@ class LanguagesPageView(View):
     def get(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect('sign_in')
+        
+        form = PhraseCreationForm()
 
+        self.context['form'] = form
         self.context['phrases'] = Phrase.objects.filter(user=request.user)
         return render(request, 'languages/languages_page.html', self.context)
+    
+    def post(self, request, *args, **kwargs):
+        if 'Create' in request.POST:
+            form = PhraseCreationForm(request.POST)
+            form.instance.user = request.user
+            if form.is_valid():
+                form.save()
+
+                return redirect('languages')
