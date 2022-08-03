c3c3ed3c697349b6aa068ab3412b75561b84cb8f
itsdaibend <kotljar.daniel@gmail.com>
2022-08-03 23:12:09 +0300

    [BE] AIOGRAM in project

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,21 +1,19 @@
 import os
 from dotenv import load_dotenv
-import telebot
-import requests
+
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+
 
 load_dotenv()
 TELEGRAM_BOT_KEY = os.environ.get('TELEGRAM_BOT_KEY')
-bot = telebot.TeleBot(TELEGRAM_BOT_KEY)
 
+bot = Bot(token=TELEGRAM_BOT_KEY)
+dp = Dispatcher(bot)
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    bot.send_message(message.chat.id, f'<b>Welcome, {message.from_user.first_name}!</b>', parse_mode='html')
+@dp.message_handler(commands=['start'])
+async def start_chat(message):
+    await message.answer( f'<b>Welcome, {message.from_user.first_name}!</b>', parse_mode='html')
 
-@bot.message_handler(commands=['phrases'])
-def phrases(message):
-    response = requests.get('http://localhost:8000/api/v1/languages/phrases', auth=('itsdaibend', '1@qWaSzX'))
-    for entity in response.json():
-        bot.send_message(message.chat.id, f'{entity["source_text"]} >>> {entity["target_text"]}', parse_mode='html')
-
-bot.polling(none_stop=True)
+executor.start_polling(dp, skip_updates=True)
