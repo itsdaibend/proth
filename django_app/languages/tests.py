44d7dece5d8809d60933c0331e5ecd3aba907bfb
Danylo Kotliar <kotljar.daniel@gmail.com>
2023-11-30 16:23:11 +0200

    [BE] Unittests and some documentation changes

--- django_app/languages/tests.py
+++ django_app/languages/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth.models import User
+from .models import Phrase
 
-# Create your tests here.
+
+class LanguagesPageViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.phrase1 = Phrase.objects.create(user=self.user, source_text='Test Source 1', target_text='Test Target 1')
+        self.phrase2 = Phrase.objects.create(user=self.user, source_text='Test Source 2', target_text='Test Target 2')
+        self.client = Client()
+
+    def test_get_languages_page_authenticated(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('languages'))
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['phrases'], map(repr, [self.phrase1, self.phrase2]), ordered=False)
+
+    def test_get_languages_page_not_authenticated(self):
+        response = self.client.get(reverse('languages'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('sign_in'))
+
+    def test_post_create_phrase(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(reverse('languages'), {'Create': '', 'source_text': 'New Source', 'target_text': 'New Target'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Phrase.objects.filter(user=self.user, source_text='New Source', target_text='New Target').count(), 1)
+
+    def test_post_delete_phrase(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(reverse('languages'), {'Delete': '', 'phrase_id': self.phrase1.id})
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Phrase.objects.filter(id=self.phrase1.id).exists())
+
+    def test_post_search_phrases(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(reverse('languages'), {'Search': 'Test Target'})
+        self.assertEqual(response.status_code, 200)
+        self.assertQuerysetEqual(response.context['phrases'], map(repr, [self.phrase1, self.phrase2]), ordered=False)
--- django_app/todo/tests.py
+++ django_app/todo/tests.py
@@ -1,3 +1,46 @@
-from django.test import TestCase
+from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
+from .models import Todo
 
-# Create your tests here.
+
+class TodoPageViewTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.client = Client()
+
+    def test_get_todo_page_authenticated(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.get(reverse('todos'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue('form' in response.context)
+        self.assertTrue('update_form' in response.context)
+        self.assertTrue('todos' in response.context)
+        self.assertQuerysetEqual(response.context['todos'], Todo.objects.filter(user=self.user).order_by('created_at'), ordered=False)
+
+    def test_get_todo_page_not_authenticated(self):
+        response = self.client.get(reverse('todos'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('sign_in'))
+
+    def test_post_create_todo_authenticated(self):
+        self.client.login(username='testuser', password='testpassword')
+        response = self.client.post(reverse('todos'), {'Create': '', 'title': 'New Todo', 'description': 'New Todo Description'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Todo.objects.filter(user=self.user, title='New Todo', description='New Todo Description').count(), 1)
+
+    def test_post_delete_todo_authenticated(self):
+        self.client.login(username='testuser', password='testpassword')
+        todo = Todo.objects.create(user=self.user, title='Test Todo', description='Test Todo Description')
+        response = self.client.post(reverse('todos'), {'Delete': '', 'todo_id': todo.id})
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(Todo.objects.filter(id=todo.id).exists())
+
+    def test_post_update_todo_authenticated(self):
+        self.client.login(username='testuser', password='testpassword')
+        todo = Todo.objects.create(user=self.user, title='Test Todo', description='Test Todo Description')
+        response = self.client.post(reverse('todos'), {'Update': '', 'todo_id': todo.id, 'title': 'Updated Todo', 'description': 'Updated Todo Description'})
+        self.assertEqual(response.status_code, 302)
+        updated_todo = Todo.objects.get(id=todo.id)
+        self.assertEqual(updated_todo.title, 'Updated Todo')
+        self.assertEqual(updated_todo.description, 'Updated Todo Description')
